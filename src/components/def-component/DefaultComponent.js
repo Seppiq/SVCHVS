191453d4676e415d7e5c3128a9fb89910c9d1180
Kirill Semak <53149697+Seppiq@users.noreply.github.com>
2023-01-12 22:58:48 +0300

    Add files via upload

---
+++ src/components/def-component/DefaultComponent.js
@@ -0,0 +1,20 @@
+import styles from './DefaultComponent.module.css'
+import {NavLink} from 'react-router-dom';
+
+
+const DefaultComponent = () => {
+    return (
+        <div className="wrapper">
+            <div className={styles.content}>
+                <div>
+                    Вы ввели несуществующий адрес
+                </div>
+                <div className={styles.button}>
+                    <NavLink to='/'>Вернуться на домашнюю страницу</NavLink>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DefaultComponent;
